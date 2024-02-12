02bda7377d4a45eae49743a8900a5aa263538b97
feylon <jamshid14092002@gmail.com>
2024-02-12 22:31:40 +0500

    22 31

--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,7 @@ router.get("/",(req, res)=>{
             title:"about"
         })
         });
-    
+     
     router.get("/products",(req, res)=>{
     res.render("products",{
         title:"products",
