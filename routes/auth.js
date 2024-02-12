b211134c068ea7202f32e2c6454727d1835b3533
feylon <jamshid14092002@gmail.com>
2024-02-12 17:49:19 +0500

    17 49

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/login",(req, res)=>{
-    res.render("login");
+    res.render("login",{
+        title:"login"
+    });
     });
 
 router.get("/registr",(req, res)=>{
-res.render("registr");
+res.render("registr",{
+    title:"Login"
+});
 });
 
 
--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,28 @@
 import { Router } from "express";
 const router = Router();
 router.get("/",(req, res)=>{
-    res.render("index")
+    res.render("index",{
+        title:"products"
+    })
     });
     
     router.get("/about",(req, res)=>{
-        res.render("about")
+        res.render("about",{
+            title:"about"
+        })
         });
     
     router.get("/products",(req, res)=>{
-    res.render("products");
+    res.render("products",{
+        title:"products",
+        isProduct:true
+    });
     });
     
     router.get("/add",(req, res)=>{
-    res.render("add");
+    res.render("add",{
+        title:"Add",
+        isAdd:true
+    });
     });
 export default router;    
