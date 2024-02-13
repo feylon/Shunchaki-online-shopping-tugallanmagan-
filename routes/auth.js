82a195a83b40361447f5978f9ab587bdedf51334
feylon <jamshid14092002@gmail.com>
2024-02-13 15:42:20 +0500

    13 dars tugatildi, paswordni heshlash jaroyoni bo'ldi

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
 const router = Router();
 
 router.get("/login",(req, res)=>{
@@ -10,6 +13,7 @@ router.get("/login",(req, res)=>{
 
 router.post("/login",(req, res)=>{
 console.log(req.body);
+
 res.redirect("/")
 });
 
@@ -20,9 +24,19 @@ res.render("registr",{
 });
 
 
-router.post("/registr",(req, res)=>{
-    console.log(req.body);
+router.post("/registr", async (req, res)=>{
+  const  hashedPassword = await bcrypt.hash(req.body.password, 10);  
+    const UserData = 
+{
+firstName : req.body.firstname,
+lastName : req.body.lastname,
+email : req.body.email,
+password : hashedPassword
+};
+console.log(UserData);
+const user = await User.create(UserData)
     res.redirect("/login")
     });
 
 export default router;
+// 3:24:05
