426089f0b23d39bcaae6444bd2df5b096cbe845d
feylon <jamshid14092002@gmail.com>
2024-02-13 15:05:08 +0500

    MongoDbga ulandik menimcha

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import  express  from "express";
 import {create} from "express-handlebars";
+import mongoose from "mongoose";
 import AuthRoutes from "./routes/auth.js";
 import ProductRoutes from "./routes/products.js"
+import * as dotenv from "dotenv" ;
+dotenv.config();
+
 const app = express();
 
 const hbs = create({
@@ -15,6 +19,7 @@ app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"))
 
 app.use(AuthRoutes);
 app.use(ProductRoutes);
@@ -22,10 +27,22 @@ app.use(ProductRoutes);
         
     
     
+const startAplication = function(){
+
+
+try {
     
+console.log(process.env.MONGO_URL)
+const PORT =  4100;
 
-    const PORT =  4100;
+mongoose.set('strictQuery',false)
 
+mongoose.connect(process.env.MONGO_URL,{});
 app.listen(PORT, ()=>{
-    console.log("Server is running  ",PORT)
-})
+console.log("Server is running  ",PORT)
+});
+} catch (error) {
+    console.log(error)
+}
+};
+startAplication();
